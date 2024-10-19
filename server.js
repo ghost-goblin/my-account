b6fbb34f0e2de652c23cb050339a29d6677a04b1
ghost-goblin <56723522+ghost-goblin@users.noreply.github.com>
2024-10-19 14:31:53 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const env = process.env.ENVIRONMENT;
 const port = process.env.APPLICATION_PORT;
 const app_name = process.env.APPLICATION_NAME;
-const secret = process.env.SECRET_01
+const secret = process.env.SECRET_01;
 
 app.get("/", (req, res) => {
   res.send(`"${env}-${app_name}" is running on port ${port}! SECRET_01="${secret}"!`);
