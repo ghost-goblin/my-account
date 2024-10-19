9adfa0f334c74f5c351625bf2644cbb7cf46a0ed
ghost-goblin <56723522+ghost-goblin@users.noreply.github.com>
2024-10-19 14:25:16 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app_name = process.env.APPLICATION_NAME;
 const secret = process.env.SECRET_01
 
 app.get("/", (req, res) => {
-  res.send(`"${env}-${app_name}" is running on port ${port}! SECRET="${secret}"!`);
+  res.send(`"${env}-${app_name}" is running on port ${port}! SECRET_01="${secret}"!`);
 });
 
 app.listen(port, () => {
