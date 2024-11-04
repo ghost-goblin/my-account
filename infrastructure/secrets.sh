ec9dcfe622d0be06869834e471510c7d14fcb0f6
puybr <56723522+puybr@users.noreply.github.com>
2024-11-04 07:37:21 +0000

    Create secrets.sh

---
+++ infrastructure/secrets.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+APPLICATION_NAME="$1"
+SECRET_ARN=$(aws secretsmanager get-secret-value --secret-id "$APPLICATION_NAME" --query ARN --output text)
+SECRET_KEYS=$(aws secretsmanager get-secret-value --secret-id "$APPLICATION_NAME" --query SecretString --output text | jq 'keys' | tr -d '[],\n\t\r"')
+ARRAY=()
+
+for key in $SECRET_KEYS
+do
+  SECRET=$(jq -n --arg key "$key" --arg arn "$SECRET_ARN:$key::" '{"name": $ARGS.named["key"],"valueFrom": $ARGS.named["arn"]}' | tr -d '\n\t\r ')
+  ARRAY+=($SECRET)
+done
+
+SECRET_LIST=$(echo "${ARRAY[@]}" | sed 's/ /,/g')
+echo $SECRET_LIST
+jq '.containerDefinitions[0].secrets += ['"$SECRET_LIST"']' infrastructure/taskdef.temp.json > taskdef.temp.secrets.json
